7830841536c62032755e143f9a7f51c6167cc77b
Ali <dotov09921@gmail.com>
2026-02-16 10:12:45 +0300

    исправление кирлицы

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 const lessons = require('./lessons');
 // const cron = require("node-cron");
-
+// A
 
 // ======================================================
 // FIREBASE
