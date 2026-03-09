93cc07654776aa45ff7ae7d1af2136927f4970f7
Ali <dotov09921@gmail.com>
2026-03-09 09:49:45 +0300

    исправление регистрации v3

--- index.js
+++ index.js
@@ -1660,5 +1660,10 @@ if (WEBHOOK_URL) {
   console.log("▶️ Запуск POLLING");
 }
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+process.once("SIGINT", () => {
+  try { bot.stop("SIGINT"); } catch(e) {}
+});
+
+process.once("SIGTERM", () => {
+  try { bot.stop("SIGTERM"); } catch(e) {}
+});
